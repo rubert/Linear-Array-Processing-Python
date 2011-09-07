50dfed6c635a025d648e92a8871a04008c94dc59
Nicholas <ncRubert@gmail.com>
2011-09-07 15:22:47 -0500

    Fixed errors in code.  Did integer division when I wanted floats.

--- faranScattering.py
+++ faranScattering.py
@@ -1,7 +1,7 @@
 class faranBsc(object):
 
 
-	 def SLtoBSC(self, x3, initFreq, stepFreq, endFreq, d, sosm = 1540.,soss = 5570.,sosshear = 3374.7,rhom = 1020.,rhos = 2540.,maxang = 180,maxn = 100)
+	def calculateBSC(self, initFreq, stepFreq, endFreq, d, sosm = 1540.,soss = 5570.,sosshear = 3374.7,rhom = 1020.,rhos = 2540.,maxang = 180,maxn = 100):
 		'''a function to calculate normalized BSC curve from Tony's Faran code. Assume Tony's 
 		%code only calculates 180 degree k3absscatlength values.
 		%
@@ -20,28 +20,24 @@ class faranBsc(object):
 		%
 		%Hairong Shi, 05/15/2006
 		'''
-		ksabsscatlength = self.sphere(sosm,soss,sosshear,rhom,rhos,maxang,maxn)
 		import numpy
+		#Work out ka values for frequency range and scatterer diameter
 		sos=1540.;   #%speed of sound in surrounding media
-
-		if x3.min()>(initFreq.*1e6)*2*numpy.pi*(d/2*1e-6)/sos or max(x3)<(endFreq*1e6)*2*numpy.pi*(d/2*1e-6)/sos 
-		    
-		    print 'Frequency out of range'
-		    return
-
 		freq=numpy.arange(initFreq, endFreq,stepFreq)*1e6;   #change unit to Hz
 		k=2*numpy.pi*freq/sos;
 		ka=k*d/2*1e-6;
-		temp=interp1(x3, k3absscatlength, ka);  
-		BSCCurve=(temp/k)**2;
+		#get scattering length and convert to backscatter coefficient	
+		kabsscatlength = self.sphere(ka,sosm,soss,sosshear,rhom,rhos,maxang,maxn)
+		BSCCurve=(kabsscatlength/k)**2;
 
 		return freq, BSCCurve
 
-	 def sphere(self,sosm,soss,sosshear,rhom,rhos,maxang,maxn)
+	def sphere(self,ka,sosm,soss,sosshear,rhom,rhos,maxang,maxn):
 
 		'''a function which calculates scattering cross sections for spheres
-		#according to Faran's theory; the output is a (1+maxang) by (size of x3) matrix
-		#where rows correspond to angles (0 to maxang) and columns correspond to different values
+		#according to Faran's theory; the output is a size of x3 vector giving
+		#the scattering length at 180 degrees
+		#and entries correspond to different values
 		#of ka (step size and max value of ka can be changed by manipulating the
 		#initialization of x3); note that the output is the absolute value of the
 		#scattering length (i.e. square root of the differential scattering cross
@@ -80,13 +76,14 @@ class faranBsc(object):
 		import numpy
 		from scipy.special import lpmn
 		#initialization
-		theta=maxang;
-		x3=.1:.1:10;  #the range of ka over which cross sections are calculated
+		theta= 180;
+		x3=ka
 		x2=x3*sosm/sosshear;  #ka value for shear waves in sphere
 		x1=x3*sosm/soss;  #ka value for compressional waves in sphere
 
 		#main loop over order n
-		for n in range(0,maxn)  	    
+		coeff = numpy.zeros((maxn + 1, len(x3)) ) + 1j*numpy.zeros((maxn + 1, len(x3)))
+		for n in range(0,maxn + 1):  	    
 		    #spherical bessel functions
 		    jx1=self.sphbess(n,x1);
 		    jx2=self.sphbess(n,x2);
@@ -96,10 +93,10 @@ class faranBsc(object):
 		    nx3=self.sphneumm(n,x3);
 		    
 		    #derivatives of spherical bessel and neumann functions
-		    jpx3=n/(2*n+1)*self.sphbess(n-1,x3)-(n+1)/(2*n+1)*self.sphbess(n+1,x3);
-		    npx3=n/(2*n+1)*self.sphneumm(n-1,x3)-(n+1)/(2*n+1)*self.sphneumm(n+1,x3);
-		    jpx1=n/(2*n+1)*self.sphbess(n-1,x1)-(n+1)/(2*n+1)*self.sphbess(n+1,x1);
-		    jpx2=n/(2*n+1)*self.sphbess(n-1,x2)-(n+1)/(2*n+1)*self.sphbess(n+1,x2);
+		    jpx3=n/(2*n+1.)*self.sphbess(n-1,x3)-(n+1)/(2*n+1.)*self.sphbess(n+1,x3);
+		    npx3=n/(2*n+1.)*self.sphneumm(n-1,x3)-(n+1)/(2*n+1.)*self.sphneumm(n+1,x3);
+		    jpx1=n/(2*n+1.)*self.sphbess(n-1,x1)-(n+1)/(2*n+1.)*self.sphbess(n+1,x1);
+		    jpx2=n/(2*n+1.)*self.sphbess(n-1,x2)-(n+1)/(2*n+1.)*self.sphbess(n+1,x2);
 		    
 		    tandeltax3=-jx3/nx3;
 		    tanalphax3=-x3*jpx3/jx3;
@@ -110,22 +107,25 @@ class faranBsc(object):
 		    #calculation of tanxsi and coefficient
 		    term1=tanalphax1/(tanalphax1+1);
 		    term2=(n**2+n)/(n**2+n-1-0.5*x2**2+tanalphax2);
-		    term3=(n**2+n-0.5*x2**2+2*tanalphax1)./(tanalphax1+1);
+		    term3=(n**2+n-0.5*x2**2+2*tanalphax1)/(tanalphax1+1);
 		    term4=((n**2+n)*(tanalphax2+1))/(n**2+n-1-0.5*x2**2+tanalphax2);
 		    tanxsi=-x2**2/2*(term1-term2)/(term3-term4);
 		    
 		    taneta=tandeltax3*(-rhom/rhos*tanxsi+tanalphax3)/(-rhom/rhos*tanxsi+tanbetax3);
-		    coeff[n+1,:]=(2*n+1)*taneta/(1+taneta**2)+i*(2*n+1)*taneta**2/(1+taneta**2);
-		    
-		    #legendre polynomials
-		    temp=legendre(n,n,numpy.cos(numpy.pi/180*theta));
-		    legend[:,n+1]=temp(1,:);
+		    coeff[n,:]=(2*n+1)*taneta/(1+taneta**2)+1j*(2*n+1)*taneta**2/(1+taneta**2);
+		   
+		#legendre polynomials
+		temp, deriv=lpmn(n,n,numpy.cos(numpy.pi/180*theta));
+		#taking the first row is effectively taking m = 0
+		legend=temp[0,:].reshape((1,maxn + 1));
+		
+		#matrix mult completes summation over n
+		k3absscatlength=abs(numpy.dot(legend,coeff));
+		output = k3absscatlength.reshape( len(x3) )
+		output[output == numpy.nan] = 0
+		return output
 
-		k3absscatlength=abs(legend*coeff);  %matrix mult completes summation over n
-		return ksabsscatlength
-
-
-	def sphneumm(self,order,vect)
+	def sphneumm(self,order,vect):
 
 		#calculates the spherical neumann function of order 'order' for the passed
 		#vector
@@ -134,10 +134,11 @@ class faranBsc(object):
 		sphneumm=numpy.sqrt(numpy.pi/2./vect)*yv(order+0.5,vect);
 		return sphneumm
 			
-	def sphbess(self,order,vect)
+	def sphbess(self,order,vect):
 		'''calculates the spherical bessel function of order 'order' for the passed
 		vector'''
 		import numpy
 		from scipy.special import jv
 
 		sphbess=numpy.sqrt(numpy.pi/2./vect)*jv(order+0.5,vect);
+		return sphbess
