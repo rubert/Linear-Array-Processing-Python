Made a while bunch of changes for data processing.  I remember fixing the generalized spectrum calculation so it relies on a chirpz transform.  Not sure what the other changes were.

blockMatch.py
@@ -7,19 +7,39 @@ import numpy as np
 
 class UniquePriorityQueue(PriorityQueue):
     def _init(self, maxsize):
+        '''This is meant to take in tuples of
+        (-quality, locInd, iniDpY, iniDpX, region)'''
         PriorityQueue._init(self, maxsize)
-        self.values = set()
+        self.locations = []
+	self.itemsInQueue = []
 
     def _put(self, item, heappush=heapq.heappush):
-        if item[1] not in self.values:
-            self.values.add(item[1])
+        #unique items added to list without checks
+	if item[1] not in self.locations:
+            self.locations.append(item[1])
+	    self.itemsInQueue.append(item)
             PriorityQueue._put(self, item, heappush)
-#        else:  I should check to see if the quality is better, if so, remove old item
-#            print 'dupe',item[1]
+	
+	#if already in list, then only insert if
+	#quality is higher than what is already in list
+        elif item[1] in self.locations:
+	    if abs(item[0]) < abs( self.itemsInQueue[ self.locations.index(item[1]) ][0] ):
+		    pass #no insertion, correlation is lower than what is already in list
+	    else:
+
+		tmpItem = self.itemsInQueue.pop( self.locations.index(item[1]) )
+		self.queue.remove( tmpItem )
+		self.locations.remove(item[1])
+
+                self.locations.append(item[1])
+	        self.itemsInQueue.append(item)
+                PriorityQueue._put(self, item, heappush)
+
 
     def _get(self, heappop=heapq.heappop):
         item = PriorityQueue._get(self, heappop)
-        self.values.remove(item[1])
+	self.itemsInQueue.pop( self.locations.index(item[1]) )
+	self.locations.remove(item[1])
         return item
 
 
@@ -27,23 +47,24 @@ class blockMatchClass(rfClass):
 
 
 	def __init__(self, fname, dataType, postFile = None,  windowYmm = 1.0, windowXmm = 10., rangeYmm = 1.0, rangeXmm = .6, overlap = .65, strainKernelmm = 6.0):
-	'''Input:
-	fname: (string)  Either a file containing a sequence of frames with motion, or a pre-compression file.
-	dataType: (string)  The filetype of the input files, see rfClass for allowed types
-	postFile: (string)  A file of post compression data, the same type and dimensions as the input file
-	windowYmm: (float) the axial window size of the block match window in mm
-	windowXmm: (float)  The lateral window size of the block match window in mm
-	rangeYmm:  (float)  The axial search range for the block match window in mm  
-	rangeXmm:  (float)  The lateral search range for the block match window in mm
-	overlap:  (float)  [0-1].  The axial overlap between block matching windows.
-	strainKernelmm:  (float)  The size of the least squares strain estimation kernel in mm.
-	
-	'''	
+		'''Input:
+		fname: (string)  Either a file containing a sequence of frames with motion, or a pre-compression file.
+		dataType: (string)  The filetype of the input files, see rfClass for allowed types
+		postFile: (string)  A file of post compression data, the same type and dimensions as the input file
+		windowYmm: (float) the axial window size of the block match window in mm
+		windowXmm: (float)  The lateral window size of the block match window in mm
+		rangeYmm:  (float)  The axial search range for the block match window in mm  
+		rangeXmm:  (float)  The lateral search range for the block match window in mm
+		overlap:  (float)  [0-1].  The axial overlap between block matching windows.
+		strainKernelmm:  (float)  The size of the least squares strain estimation kernel in mm.
+		
+		'''	
 		super(blockMatchClass, self).__init__(fname, dataType)	
 		
 		if postFile:
 			self.postRf = rfClass(postFile, dataType)
-
+		else:
+			self.postRf = None
 		self.windowYmm = windowYmm	
 		self.windowXmm = windowXmm
 		self.rangeYmm =  rangeYmm
@@ -99,6 +120,10 @@ class blockMatchClass(rfClass):
 		self.halfLsq = self.strainWindow//2	
 		self.strainwindowmm = self.strainWindow*self.deltaY*self.stepY
 			
+	def InitializeArrays(self):
+		'''Called to clear out displacement and quality arrays if
+		I'm looking at a sequence of strain images.'''
+	
 	######allocate numpy arrays to store quality, dpy, dpx
 		self.dpY = np.zeros( (self.numY, self.numX) )
 		self.dpX = np.zeros( (self.numY, self.numX) )
@@ -128,7 +153,6 @@ class blockMatchClass(rfClass):
 	#for determining bad seeds
 		self.threshold = round( (self.numY/10)*(self.numX/10) )
 
-
 	def CreateStrainImage(self, preFrame = 0, postFrame = 1, vMax = .02):
 		'''With the given parameters, pre, and post RF data create a strain image.'''
 
@@ -136,12 +160,14 @@ class blockMatchClass(rfClass):
 		self.ReadFrame(preFrame)
 		self.pre = self.data.copy()
 		
-		if self.postFile:
+		if self.postRf:
 			self.postRf.ReadFrame(preFrame)
 			self.post = self.postRf.data.copy()
 		else:
 			self.ReadFrame(postFrame)
 			self.post = self.data.copy()
+
+		self.InitializeArrays()
 		#Perform tracking
 		self.TrackSeeds()
 		self.TrackNonSeeds()
@@ -150,12 +176,16 @@ class blockMatchClass(rfClass):
 
 		#take the strain image and create a scan-converted strain image.
 		startY =self.windowCenterY[self.halfLsq]
+		startYdp = self.windowCenterY[0]
 		startX =self.windowCenterX[0]
 		stepY =self.stepY
 		stepX =1
 
 		self.strain[self.strain> vMax] = vMax
 		self.strainRGB = self.CreateParametricImage(self.strain,[startY, startX], [stepY, stepX], colormap = 'gray' )
+		self.dpYRGB = self.CreateParametricImage(self.dpY,[startYdp, startX], [stepY, stepX] )
+		self.dpXRGB = self.CreateParametricImage(self.dpX,[startYdp, startX], [stepY, stepX])
+		self.qualityRGB = self.CreateParametricImage(self.quality,[startYdp, startX], [stepY, stepX] )
 
 	
 	def Sub2ind(self,shape, row, col):
@@ -204,7 +234,6 @@ class blockMatchClass(rfClass):
 
 	def TrackSeeds(self):
 		"""Perform block-matching only on the seed points """
-		#pdb.set_trace()
 	#allocate array to hold results of cross correlation
 		resultNp = np.float32( np.zeros( (2*self.rangeY + 1, 2*self.rangeX + 1) ) )
 		resultCv = cv.fromarray(resultNp)
@@ -227,9 +256,9 @@ class blockMatchClass(rfClass):
 				template = cv.fromarray( np.float32(self.pre[startBlockY:stopBlockY, startBlockX:stopBlockX ] )  )
 				
 				startBlockY = self.windowCenterY[y] - self.halfY - self.rangeY
-				stopBlockY = self.windowCenterY[y] + self.halfY + self.rangeY + 1
+				stopBlockY = self.windowCenterY[y] + self.halfY + 1 + self.rangeY 
 				startBlockX = self.windowCenterX[x] - self.halfX - self.rangeX
-				stopBlockX = self.windowCenterX[x] + self.halfX + self.rangeX + 1
+				stopBlockX = self.windowCenterX[x] + self.halfX + 1 + self.rangeX 
 				image = cv.fromarray( np.float32( self.post[startBlockY:stopBlockY, startBlockX:stopBlockX]  )   )
 				
 				cv.MatchTemplate(template, image, resultCv, cv.CV_TM_CCORR_NORMED )
@@ -293,9 +322,9 @@ class blockMatchClass(rfClass):
 			template = cv.fromarray( np.float32(self.pre[startBlockY:stopBlockY, startBlockX:stopBlockX ] )  )
 			
 			startBlockY = self.windowCenterY[y] - self.halfY - self.smallRangeY + iniDpY
-			stopBlockY = self.windowCenterY[y] + self.halfY + self.smallRangeY + iniDpY + 1
+			stopBlockY = self.windowCenterY[y] + self.halfY + self.smallRangeY + 1 + iniDpY 
 			startBlockX = self.windowCenterX[x] - self.halfX - self.smallRangeX + iniDpX
-			stopBlockX = self.windowCenterX[x] + self.halfX + self.smallRangeX + iniDpX + 1
+			stopBlockX = self.windowCenterX[x] + self.halfX + self.smallRangeX + 1 + iniDpX
 			image = cv.fromarray( np.float32( self.post[startBlockY:stopBlockY, startBlockX:stopBlockX]  )   )
 			
 			cv.MatchTemplate(template, image, resultCv, cv.CV_TM_CCORR_NORMED )
@@ -339,7 +368,7 @@ class blockMatchClass(rfClass):
 			intDpX = int(round(self.dpX[y,x]))
 
 			#PUT ITEM IN SEED LIST
-			self.AddToSeedList(maxCC, y,x, intDpY,intDpX, region) 
+			self.AddToSeedList(maxCC, y, x, intDpY,intDpX, region) 
 
 
 
@@ -393,9 +422,9 @@ class blockMatchClass(rfClass):
 				template = cv.fromarray( np.float32(self.pre[startBlockY:stopBlockY, startBlockX:stopBlockX ] )  )
 				
 				startBlockY = self.windowCenterY[y] - self.halfY - self.smallRangeY + iniDpY
-				stopBlockY = self.windowCenterY[y] + self.halfY + self.smallRangeY + iniDpY + 1
+				stopBlockY = self.windowCenterY[y] + self.halfY + self.smallRangeY + 1 + iniDpY
 				startBlockX = self.windowCenterX[x] - self.halfX - self.smallRangeX + iniDpX
-				stopBlockX = self.windowCenterX[x] + self.halfX + self.smallRangeX + iniDpX + 1
+				stopBlockX = self.windowCenterX[x] + self.halfX + self.smallRangeX + 1 + iniDpX
 				image = cv.fromarray( np.float32( self.post[startBlockY:stopBlockY, startBlockX:stopBlockX]  )   )
 				
 				cv.MatchTemplate(template, image, resultCv, cv.CV_TM_CCORR_NORMED )

generalizedSpectrum.py
@@ -2,7 +2,7 @@ from rfData import rfClass
 
 class collapsedAverageImage(rfClass):
 
-	def ComputeCollapsedAverageImage(self, windowYmm = 8, windowXmm = 8, overlapY = .75, overlapX = .75):
+	def ComputeCollapsedAverageImage(self, freqLowMHz = 2.0, freqHighMHz = 12.0, windowYmm = 8, windowXmm = 8, overlapY = .75, overlapX = .75, vMax = None):
 		'''Using 4 mm by 4 mm windows, create a collapsed average image.'''
 
 		import numpy
@@ -21,7 +21,7 @@ class collapsedAverageImage(rfClass):
 		halfY = (windowY -1)/2
 		halfX = (windowX -1)/2
 			
-		#overlap the windows axially by 50%
+		#overlap the windows axially and laterally
 		stepY = int(  (1-overlapY)*windowY )
 		startY = halfY
 		stopY = self.points - halfY - 1
@@ -37,7 +37,25 @@ class collapsedAverageImage(rfClass):
 		self.caImage = numpy.zeros( (numY, numX) )
 		self.caStepX = stepX
 		self.caStepY = stepY
-			
+
+		#Work out parameters for chirpz transform
+		self.freqHigh = freqHighMHz
+		self.freqLow = freqLowMHz	
+		
+		#figure out block size in points
+		self.bartlettY = 2*halfY
+		self.bartlettY -= self.bartlettY%4
+		self.bartlettY /= 2
+		freqStep = (freqHighMHz - freqLowMHz)/self.bartlettY
+		freqStepLowRes = self.fs/(10.**6*self.bartlettY)
+		self.spectrumFreqLowRes	= numpy.arange(self.bartlettY)*freqStepLowRes
+
+		self.spectrumFreq = numpy.arange(0,self.bartlettY)*freqStep  + freqLowMHz	
+		fracUnitCircle = (freqHighMHz - freqLowMHz)/(self.fs/10**6)
+		self.cztW = numpy.exp(1j* (-2*numpy.pi*fracUnitCircle)/self.bartlettY ) 
+		self.cztA = numpy.exp(1j* (2*numpy.pi*freqLowMHz/(self.fs/10**6) ) )
+
+		
 		#work out time to compute a point, then spit out time to calculate image
 		from time import time
 		y = x = 0
@@ -54,84 +72,102 @@ class collapsedAverageImage(rfClass):
 				self.CalculateGeneralizedSpectrum(region = tempRegion)
 				self.caImage[y,x] = self.areaUnderCA
 		
-			
-		self.caImage = self.CreateParametricImage(self.caImage,[startY, startX], [self.caStepY, self.caStepX] )	
+		if vMax:
+			self.caImage[self.caImage > vMax] = vMax	
+		self.caImageRGB = self.CreateParametricImage(self.caImage,[startY, startX], [self.caStepY, self.caStepX] )	
 	
 	
 	def CalculateGeneralizedSpectrum(self, region):
-		'''Accept a block of data and compute the collapsed average in the region.'''
+		'''Accept a block of data and compute the collapsed average in the region.
+		Average over 3 sub-blocks to compute GS'''
 		
-		import numpy	
-		from scipy.signal import hamming
-
+		from scipy.signal import hann,convolve
+		import numpy
+		from chirpz import chirpz
 		points = region.shape[0]
 		points -= points%4
-		maxDataWindow = region[0:points, :]
 		points /= 2
 		
+		maxDataWindow = region[0:2*points, :]
+
+		#compute 3 fourier transforms and average them	
+		#Cutting off the zero-value end points of the hann window
+		#so it matches Matlab's definition of the function
+		windowFunc = hann(points+2)[1:-1].reshape(points,1)
+	
+		##############
+		###BLOCK 1####
+		##############
 		#Work out the delay between the first point and the maximum intensity point
-		windowFunc = hamming(points).reshape(points,1)
 		dataWindow = maxDataWindow[0:points, :]*windowFunc	
-		maxPointDelay = dataWindow.argmax(axis = 0 )/self.fs
-		fourierData =numpy.fft.fft(dataWindow, axis = 0)
 		tempPoints = dataWindow.shape[0]
 		tempLines = dataWindow.shape[1]
 		
+		fourierData = numpy.zeros( dataWindow.shape ) + 1j*numpy.zeros( dataWindow.shape)
+		for l in range(tempLines):	
+			fourierData[:,l] = chirpz(dataWindow[:,l], self.cztA, self.cztW, points)
+		
+	
+		#get point delay in seconds	
+		maxPointDelay = dataWindow.argmax(axis = 0 )/self.fs
 		#Work out frequency spacing for DFT points
-		deltaF = self.fs/dataWindow.shape[0]
-		freq =numpy.arange(0, self.fs, deltaF)
-		delta = numpy.outer(freq,maxPointDelay)
+		delta = numpy.outer(self.spectrumFreq*10**6,maxPointDelay)
 		phase = numpy.exp(1j*2*numpy.pi*delta)	
 		gsList1 = []
 		
 		for l in range(tempLines):
 			outerProd = numpy.outer(fourierData[:,l]*phase[:,l], fourierData[:,l].conjugate()*phase[:,l].conjugate() )
-			outerProd[abs(outerProd) < 1E-8] =  0. + 1j*0.
 			outerProd /= abs(outerProd)
 			outerProd[outerProd == numpy.nan] = 0. + 1j*0.
 			gsList1.append(outerProd.copy() )
 
+		############
+		###BLOCK 2##
+		############
 		#step ahead by 50% the window size and add in more contributions
 		dataWindow = maxDataWindow[points/2: points/2 + points, :]*windowFunc
-		
-		maxPointDelay = dataWindow.argmax(axis = 0 )/self.fs
-		fourierData =numpy.fft.fft(dataWindow, axis = 0)
 		tempPoints = dataWindow.shape[0]
 		tempLines = dataWindow.shape[1]
+		fourierData = numpy.zeros( dataWindow.shape ) + 1j*numpy.zeros( dataWindow.shape)
+		for l in range(tempLines):	
+			fourierData[:,l] = chirpz(dataWindow[:,l], self.cztA, self.cztW, points)
+	
+		maxPointDelay = dataWindow.argmax(axis = 0 )/self.fs
+		
 
 		#Work out frequency spacing for DFT points
-		deltaF = self.fs/dataWindow.shape[0]
-		freq = numpy.arange(0, self.fs, deltaF)
-		delta = numpy.outer(freq,maxPointDelay)
+		deltaF = self.spectrumFreq[1] - self.spectrumFreq[0]
+		delta = numpy.outer(self.spectrumFreq*10**6,maxPointDelay)
 		phase = numpy.exp(1j*2*numpy.pi*delta)	
 	
 		gsList2 = []	
 		for l in range(tempLines):
 			outerProd = numpy.outer(fourierData[:,l]*phase[:,l], fourierData[:,l].conjugate()*phase[:,l].conjugate() )
-			outerProd[abs(outerProd) < 1E-8] =  0. + 1j*0.
 			outerProd /= abs(outerProd)
 			outerProd[outerProd == numpy.nan] = 0. + 1j*0.
 			gsList2.append(outerProd)
 
 
+		############
+		###BLOCK 3##
+		############
 		#step ahead by 50% the window size and add in more contributions
 		dataWindow = maxDataWindow[points:2*points, :]*windowFunc		
-		
-		maxPointDelay = dataWindow.argmax(axis = 0 )/self.fs
-		fourierData =numpy.fft.fft(dataWindow, axis = 0)
 		tempPoints = dataWindow.shape[0]
 		tempLines = dataWindow.shape[1]
+		fourierData = numpy.zeros( dataWindow.shape ) + 1j*numpy.zeros( dataWindow.shape)
+		for l in range(tempLines):	
+			fourierData[:,l] = chirpz(dataWindow[:,l], self.cztA, self.cztW, points)
 
+		maxPointDelay = dataWindow.argmax(axis = 0 )/self.fs
 		#Work out frequency spacing for DFT points
-		deltaF = self.fs/dataWindow.shape[0]
-		freq = numpy.arange(0, self.fs, deltaF)
-		delta = numpy.outer(freq,maxPointDelay)
+		deltaF = self.spectrumFreq[1] - self.spectrumFreq[0]
+		delta = numpy.outer(self.spectrumFreq*10**6,maxPointDelay)
 		phase = numpy.exp(1j*2*numpy.pi*delta)	
 	
 		gsList3 = []	
 		for l in range(tempLines):
 			outerProd = numpy.outer(fourierData[:,l]*phase[:,l], fourierData[:,l].conjugate()*phase[:,l].conjugate() )
-			outerProd[abs(outerProd) < 1E-8] =  0. + 1j*0.
 			outerProd /= abs(outerProd)
 			outerProd[outerProd == numpy.nan] = 0. + 1j*0.
 			gsList3.append(outerProd)
@@ -140,6 +176,7 @@ class collapsedAverageImage(rfClass):
 		for l in range(tempLines):
 			GS += gsList1[l] + gsList2[l] + gsList3[l]
 
+		
 		######################################################
 		########scale GS by its standard deviation############
 		######################################################
@@ -150,29 +187,21 @@ class collapsedAverageImage(rfClass):
 		
 		SL = numpy.sqrt( (1/(tempPoints*tempLines*3 - 1) )*SL )
 
-		for f1 in range(len(freq)):
-			for f2 in range(len(freq)):	
+		for f1 in range(len(self.spectrumFreq)):
+			for f2 in range(len(self.spectrumFreq)):	
 				if abs(SL[f1,f2]) > 1.E-8:
 					GS[f1,f2] /= abs(SL[f1,f2])	
 	
-		#only show min:max MHz
-		minFMHz = 5.
-		maxFMHz = 13.
-		fIndexMax = int(maxFMHz*1.E6/deltaF)
-		fIndexMin = int(minFMHz*1.E6/deltaF)
-				
-		gsDb = abs(GS)
-		gsDb -= gsDb.max()
 
 
 		########################################
 		########COMPUTE COLLAPSED AVERAGE#######
 		########################################
-		numFreq = len(range(fIndexMin, fIndexMax))	
+		numFreq = len(self.spectrumFreq)	
 		counts = numpy.zeros(numFreq)
 		CA = numpy.zeros(numFreq) + 1j*numpy.zeros(numFreq)
-		for f1 in range(fIndexMin,fIndexMax):
-			for f2 in range(fIndexMin, fIndexMax):
+		for f1 in range(numFreq):
+			for f2 in range(numFreq):
 				d = abs(f2-f1)		
 				if d < numFreq:
 					CA[d] += GS[f1,f2]
@@ -180,14 +209,9 @@ class collapsedAverageImage(rfClass):
 
 
 		self.CA = abs(CA)/counts
-		self.freqDiff = numpy.arange(0, numFreq)*deltaF/1.E6
-
-		#normalize to value at origin
-		self.CA = self.CA/self.CA[0]
 
 		#compute area under the collapsed average curve
 		self.areaUnderCA = 0.
 
 		for point in range(len(self.CA) - 1):
-			self.areaUnderCA += (self.CA[point] + self.CA[point + 1])/2*deltaF/1.E6
-
+			self.areaUnderCA += (self.CA[point] + self.CA[point + 1])/2

scatterSizeImage.py
@@ -2,20 +2,20 @@ from rfData import rfClass
 from attenuation import attenuation
 from faranScattering import faranBsc
 
-class scattererSizeClass(attenuation):
+class scatterSizeClass(attenuation):
 	
-	def __init__(self, sampleName, refName, dataType, bscCoefficients, startFreqBsc, stepFreq, numRefFrames = 0, refAttenuation = .5, freqLow = 2., freqHigh = 8., attenuationKernelSizeYmm = 15, blockYmm = 8, blockXmm = 10, overlapY = .85, overlapX = .85, frequencySmoothingKernel = .25, centerFreqSimulation = 5.0, sigmaSimulation = 1.0 ):
+	def __init__(self, sampleName, refName, dataType, bscCoefficients, bscFreqArray, numRefFrames = 0, refAttenuation = .5, freqLow = 2., freqHigh = 8., attenuationKernelSizeYmm = 15, blockYmm = 8, blockXmm = 10, overlapY = .85, overlapX = .85, bscFitRadius = 1.0, centerFreqSimulation = 5.0, sigmaSimulation = 1.0 ):
 		
 		'''The additional information this code needs on top of attenuation is a set of theoretical
 		backscatter coefficients to match against, and the backscatter coefficients of the
 		reference phantom.'''
 		
 			
-		super(scattererSizeClass, self).__init__(sampleName, refName, dataType,  numRefFrames, refAttenuation, freqLow, freqHigh, attenuationKernelSizeYmm, blockYmm, blockXmm, overlapY , overlapX , frequencySmoothingKernel, centerFreqSimulation, sigmaSimulation )				
+		super(scatterSizeClass, self).__init__(sampleName, refName, dataType,  numRefFrames, refAttenuation, freqLow, freqHigh, attenuationKernelSizeYmm, blockYmm, blockXmm, overlapY , overlapX , bscFitRadius, centerFreqSimulation, sigmaSimulation )				
 		#Get backscatter coefficients of the reference phantom in the same analysis range as the frequency Kernel
 		import numpy
 		from scipy.interpolate import interp1d
-		bscNew = interp1d(numpy.arange(0,len(bscCoefficients))*stepFreq + startFreqBsc, bscCoefficients)
+		bscNew = interp1d(bscFreqArray, bscCoefficients)
 		self.bscReference = bscNew(self.spectrumFreq)
 
 		self.bscFaranSizes = numpy.arange(1,150)
@@ -31,7 +31,7 @@ class scattererSizeClass(attenuation):
 		import numpy
 
 		self.CalculateAttenuationImage()
-		
+				
 		self.InitializeTheoreticalBackscatter()
 		numY = len(self.blockCenterY)
 		numX = len(self.blockCenterX)
@@ -55,7 +55,14 @@ class scattererSizeClass(attenuation):
 				betadiff /= 8.686
 				depthCm = (self.deltaY*yRf)/10
 				rpmSpectrum = self.sampleSpectrum[:,yParam,xParam]/self.refSpectrum[:,yParam]*numpy.exp(4*betadiff*depthCm*self.spectrumFreq)*self.bscReference
-				diffs = self.CompareBscCoefficients(rpmSpectrum )		
+				middleInd = self.refSpectrum[:,yParam].argmax()
+				lowInd = middleInd - self.radiusInPoints
+				if lowInd < 0:
+					lowInd = 0
+				highInd = middleInd + self.radiusInPoints
+				if highInd > self.refSpectrum.shape[0]:
+					highInd = self.refSpectrum.shape[0]
+				diffs = self.CompareBscCoefficients(rpmSpectrum , lowInd, highInd)		
 				self.scatSizeImage[yParam, xParam] = self.bscFaranSizes[diffs.argmin()]	
 			
 		print "Mean Scatterer size is: " + str(self.scatSizeImage.mean())	
@@ -85,7 +92,7 @@ class scattererSizeClass(attenuation):
 			self.bscCurveFaranList.append( tempBsc.copy())
 
 	
-	def CompareBscCoefficients(self, BSCs):
+	def CompareBscCoefficients(self, BSCs, startIndex, stopIndex):
 		'''Compute a theoretical backscatter curve over the transducer bandwidth.  Find the 
 		logarithm of the difference between the two curves. 
 		 
@@ -95,13 +102,18 @@ class scattererSizeClass(attenuation):
 			
 		Output:  Error and corresponding scatterer sizes'''
 		import numpy
-		
-		
+	
+		if startIndex < 0:
+			startIndex = 0
+		if stopIndex > len(BSCs):
+			stopIndex = len(BSCs)
+				
+		#Work out start index and stop index	
 		mmse = numpy.zeros( len(self.bscCurveFaranList) )
 		for count,BSCt in enumerate(self.bscCurveFaranList):
 
 			#Added small number to avoid taking logarithm of zero
-			psi = 10*numpy.log(BSCs) - 10*numpy.log(BSCt)
+			psi = 10*numpy.log(BSCs[startIndex:stopIndex]) - 10*numpy.log(BSCt[startIndex:stopIndex])
 			psiHat = psi.mean()
 			mmse[count] = ((psi - psiHat)**2).mean()
 